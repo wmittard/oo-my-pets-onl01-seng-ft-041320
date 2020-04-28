8211c8e42782eebf9e79680838c9ccf7727139d5
William Mittard <billymittard@ymail.com>
2020-04-28 21:11:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
-  # code goes here
-end
+attr_reader :name
+attr_accessor :mood, :owner 
+
+@@all = []
+
+def initialize(name,owner,mood="nervous")
+  @name = name 
+  @owner = owner 
+  @mood = mood
+  @@all << self 
+end 
+
+def self.all 
+  @@all 
+end 
+
+end 
+
+
